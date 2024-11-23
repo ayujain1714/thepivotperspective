0b57a16d7c9ae0ff0f560bfbb800f441c659e69a
Ayush Jain <154670600+ayujain1714@users.noreply.github.com>
2024-11-23 08:15:06 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,18 +73,18 @@ export default {
         sans: 'var(--font-hubotSans)',
       },
 	  keyframes: {
-        vibrate: {
-          "0%": { transform: "translate(-1px, 2px) rotate(0deg)" },
-          "10%": { transform: "translate(1px, -3px) rotate(1deg)" },
-          "20%": { transform: "translate(-2px, 1px) rotate(-1deg)" },
-          "30%": { transform: "translate(1px, 1px) rotate(0deg)" },
-          "40%": { transform: "translate(2px, -2px) rotate(-5deg)" },
-          "60%": { transform: "translate(-1px, -2px) rotate(1deg)" },
-          "70%": { transform: "transform: translate(0px, 2px) rotate(0deg)" },
-          "80%": { transform: "translate(-1px, 2px) rotate(1deg)" },
-          "90%": { transform: "translate(0px, -3px) rotate(2deg)" },
-          "100%": { transform: "translate(-3px, 1px) rotate(-2deg)" },
-        },
+        // vibrate: {
+        //   "0%": { transform: "translate(-1px, 2px) rotate(0deg)" },
+        //   "10%": { transform: "translate(1px, -3px) rotate(1deg)" },
+        //   "20%": { transform: "translate(-2px, 1px) rotate(-1deg)" },
+        //   "30%": { transform: "translate(1px, 1px) rotate(0deg)" },
+        //   "40%": { transform: "translate(2px, -2px) rotate(-5deg)" },
+        //   "60%": { transform: "translate(-1px, -2px) rotate(1deg)" },
+        //   "70%": { transform: "transform: translate(0px, 2px) rotate(0deg)" },
+        //   "80%": { transform: "translate(-1px, 2px) rotate(1deg)" },
+        //   "90%": { transform: "translate(0px, -3px) rotate(2deg)" },
+        //   "100%": { transform: "translate(-3px, 1px) rotate(-2deg)" },
+        // },
         moveUp: {
           '0%': { transform: 'translateY(5%)', opacity: '0' },
           '100%': { transform: 'translateY(0%)', opacity: '1' }
@@ -95,7 +95,7 @@ export default {
         }
       },
       animation: {
-        vibrate: 'vibrate 3s ease infinite',
+        // vibrate: 'vibrate 3s ease infinite',
         moveUp: 'moveUp 1.4s ease forwards',
         appear: 'appear 1s 0.2s forwards'
       }
